8e11d02f83f17d620e6df4b0ae255f1382dcd0cf
Michelle Harrop <harrop.michelle@gmail.com>
2019-09-12 11:44:11 -0400

    made index and login pages

--- demo/app.js
+++ demo/app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
 
 var app = express();
 
@@ -20,12 +21,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
 // error handler
 app.use(function(err, req, res, next) {
---
+++ demo/public/javascripts/indexscripts.js
@@ -0,0 +1,9 @@
+"use strict"
+
+$(function ()
+{
+    $("#login").on("click", function ()
+    {
+        window.location.href = "http://127.0.0.1:3000/login"
+    })
+})
